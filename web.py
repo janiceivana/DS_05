8b7a0362b828a82125f15e98862f47110fa9456c
Janice Ivana <jiva0001@student.monash.edu>
2024-04-15 21:47:02 +1000

    Update web.py

--- web.py
+++ web.py
@@ -19,11 +19,11 @@ alt.themes.enable("dark")
 
 #######################
 # Load data
-calendar = pd.read_csv("calendar.csv")
-evaluation = pd.read_csv("sales_train_evaluation.csv")
-validation = pd.read_csv("sales_train_validation.csv")
-prices = pd.read_csv("sell_prices.csv")
-sample = pd.read_csv("sample_submission.csv")
+# calendar = pd.read_csv("calendar.csv")
+# evaluation = pd.read_csv("sales_train_evaluation.csv")
+# validation = pd.read_csv("sales_train_validation.csv")
+# prices = pd.read_csv("sell_prices.csv")
+# sample = pd.read_csv("sample_submission.csv")
 
 # hobbies_1 = pd.read_csv('hobbies_1.csv')
 # hobbies_2 = pd.read_csv('hobbies_2.csv')
@@ -33,13 +33,27 @@ sample = pd.read_csv("sample_submission.csv")
 # food_2 = pd.read_csv('food_2.csv')
 # food_3 = pd.read_csv('food_3.csv')
 
-h1_df = pd.read_csv('h1_df.csv')
-h2_df= pd.read_csv('h2_df.csv')
-ho1_df = pd.read_csv('ho1_df.csv')
-ho2_df = pd.read_csv('ho2_df.csv')
-f1_df = pd.read_csv('f1_df.csv')
-f2_df = pd.read_csv('f2_df.csv')
-f3_df = pd.read_csv('f3_df.csv')
+# h1_df = pd.read_csv('h1_df.csv')
+# h2_df= pd.read_csv('h2_df.csv')
+# ho1_df = pd.read_csv('ho1_df.csv')
+# ho2_df = pd.read_csv('ho2_df.csv')
+# f1_df = pd.read_csv('f1_df.csv')
+# f2_df = pd.read_csv('f2_df.csv')
+# f3_df = pd.read_csv('f3_df.csv')
+
+
+calendar = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\calendar.csv')
+f1_df = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\f1_df.csv')
+f2_df = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\f2_df.csv')
+f3_df = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\f3_df.csv')
+h1_df = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\h1_df.csv')
+h2_df = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\h2_df.csv')
+ho1_df = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\ho1_df.csv')
+ho2_df = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\ho2_df.csv')
+evaluation = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\sales_train_evaluation.csv')
+validation = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\sales_train_validation.csv')
+sample = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\sample_submission.csv')
+prices = pd.read_csv('D:\\YEAR 3\\Semester 2\\backup\\sell_prices.csv')
 
 department_data = {
     'FOODS_3': f3_df,
